2d6c3823f4bd9153e63dbfc5d02b52ce225c3d16
Kathryn Iverson <kd.iverson@gmail.com>
2015-03-06 13:46:39 -0500

    make functions for bowtie and blast

--- code/hmpOPFs.bash
+++ code/hmpOPFs.bash
@@ -24,6 +24,49 @@ vaginal_introitus)
 READS_LOC="ftp://public-ftp.hmpdacc.org/Illumina/"
 GENES_LOC="ftp://public-ftp.hmpdacc.org/HMGI/"
 
+function fun_blast{
+	bzcat *_aa.fasta.bz2 > all.aa.genes.fasta && python ~/scripts/removeShortContigs.py all.aa.genes.fasta all.aa.genes100.fasta 100
+	formatdb -i all.aa.genes100.fasta -p T -n all.aa.genes100.fasta.blastDB
+	blastp -query all.aa.genes100.fasta -db all.aa.genes100.fasta.blastDB -out allvall.aa.genes100.out -evalue 1e-5 -outfmt 6 -max_target_seqs 10000 -num_threads 8
+
+}
+
+function fun_bowtie{
+	bzcat *_nucleotide.fasta.bz2 > all.nucleotide.genes.fasta && python ~/scripts/removeShortContigs.py all.nucleotide.genes.fasta all.nucleotide.genes300.fasta 300
+
+	bowtie-build all.nucleotide.genes300.fasta all.nucleotide.genes300.fasta.bowtieDB
+
+	READS=$1
+	BASE=$(basename $READS .tar.bz2)
+
+	bzcat $READS | tar -xvf -
+
+	bowtie ../genes/all.nucleotide.genes300.fasta.bowtieDB -1 $BASE/$BASE.denovo_duplicates_marked.trimmed.1.fastq -2 $BASE/$BASE.denovo_duplicates_marked.trimmed.2.fastq -p 16 -S $BASE.pe.aligned2genes.sam
+
+	samtools view -bS $BASE.pe.aligned2genes.sam > $BASE.pe.aligned2genes.bam
+	rm $BASE.pe.aligned2genes.sam
+	samtools sort $BASE.pe.aligned2genes.bam $BASE.pe.aligned2genes.sorted
+	rm $BASE.pe.aligned2genes.bam
+
+	samtools index $BASE.pe.aligned2genes.sorted.bam
+	samtools idxstats $BASE.pe.aligned2genes.sorted.bam > $BASE.pe.mapped2genes.txt
+	rm $BASE.pe.aligned2genes.sorted.bam
+
+
+	bowtie ../genes/all.nucleotide.genes300.fasta.bowtieDB -q $BASE/$BASE.denovo_duplicates_marked.trimmed.singleton.fastq -p 16 -S $BASE.se.aligned2genes.sam
+
+	samtools view -bS $BASE.se.aligned2genes.sam > $BASE.se.aligned2genes.bam
+	rm $BASE.se.aligned2genes.sam
+	samtools sort $BASE.se.aligned2genes.bam $BASE.se.aligned2genes.sorted
+	rm $BASE.se.aligned2genes.bam
+
+	samtools index $BASE.se.aligned2genes.sorted.bam
+	samtools idxstats $BASE.se.aligned2genes.sorted.bam > $BASE.se.mapped2genes.txt
+	rm $BASE.se.aligned2genes.sorted.bam
+
+	rm -rf $BASE
+}
+
 
 #parallelize this
 for i in ${DATASETS[*]};
@@ -33,50 +76,16 @@ do
 
 	wget ${READS_LOC}${i}/* -P $i/rawreads
 	wget ${GENES_LOC}${i}/* -P $i/genes
+
+	cd $i/rawreads
+	for f in *.tar.bz2
+	do
+		fun_bowtie
+	done
+
+	cd $i/genes
+	fun_blast
 done;
-
-
-#everything past here is the same for all datasets
-
-
-bzcat *_aa.fasta.bz2 > all.aa.genes.fasta && python ~/scripts/removeShortContigs.py all.aa.genes.fasta all.aa.genes100.fasta 100
-formatdb -i all.aa.genes100.fasta -p T -n all.aa.genes100.fasta.blastDB
-blastp -query all.aa.genes100.fasta -db all.aa.genes100.fasta.blastDB -out allvall.aa.genes100.out -evalue 1e-5 -outfmt 6 -max_target_seqs 10000 -num_threads 8
-
-bzcat *_nucleotide.fasta.bz2 > all.nucleotide.genes.fasta && python ~/scripts/removeShortContigs.py all.nucleotide.genes.fasta all.nucleotide.genes300.fasta 300
-
-bowtie-build all.nucleotide.genes300.fasta all.nucleotide.genes300.fasta.bowtieDB
-
-
-READS=$1
-BASE=$(basename $READS .tar.bz2)
-
-bzcat $READS | tar -xvf -
-
-bowtie ../genes/all.nucleotide.genes300.fasta.bowtieDB -1 $BASE/$BASE.denovo_duplicates_marked.trimmed.1.fastq -2 $BASE/$BASE.denovo_duplicates_marked.trimmed.2.fastq -p 16 -S $BASE.pe.aligned2genes.sam
-
-samtools view -bS $BASE.pe.aligned2genes.sam > $BASE.pe.aligned2genes.bam
-rm $BASE.pe.aligned2genes.sam
-samtools sort $BASE.pe.aligned2genes.bam $BASE.pe.aligned2genes.sorted
-rm $BASE.pe.aligned2genes.bam
-
-samtools index $BASE.pe.aligned2genes.sorted.bam
-samtools idxstats $BASE.pe.aligned2genes.sorted.bam > $BASE.pe.mapped2genes.txt
-rm $BASE.pe.aligned2genes.sorted.bam
-
-
-bowtie ../genes/all.nucleotide.genes300.fasta.bowtieDB -q $BASE/$BASE.denovo_duplicates_marked.trimmed.singleton.fastq -p 16 -S $BASE.se.aligned2genes.sam
-
-samtools view -bS $BASE.se.aligned2genes.sam > $BASE.se.aligned2genes.bam
-rm $BASE.se.aligned2genes.sam
-samtools sort $BASE.se.aligned2genes.bam $BASE.se.aligned2genes.sorted
-rm $BASE.se.aligned2genes.bam
-
-samtools index $BASE.se.aligned2genes.sorted.bam
-samtools idxstats $BASE.se.aligned2genes.sorted.bam > $BASE.se.mapped2genes.txt
-rm $BASE.se.aligned2genes.sorted.bam
-
-rm -rf $BASE
 
 
 python ../../opf/code/makecountfilefrombowtie.py -f ../genes/all.aa.genes100.fasta -o saliva.genes.count -l *.txt
